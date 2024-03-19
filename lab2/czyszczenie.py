d8acb178b3a291dc8402b51811116d18f203fc1c
lminasiewicz <l.minasiewicz.027@studms.ug.edu.pl>
2024-03-19 04:03:00 +0100

    did a little progress on lab2 and gave up

--- lab2/czyszczenie.py
+++ lab2/czyszczenie.py
@@ -5,11 +5,11 @@ import math
 def numerize(data: pd.DataFrame) -> pd.DataFrame:
     df = data.copy(deep=True)
     for val in df.values[0]:
-        if type(val) == "str":
+        if isinstance(val, str):
             if val.isnumeric():
                 val = float(val)
     for val in df.values[1]:
-        if type(val) == "str":
+        if isinstance(val, str):
             if val.isnumeric():
                 val = float(val)
     return df
@@ -19,12 +19,8 @@ def check_correctness(arr: np.ndarray) -> bool:
     if len(arr) != 5: return False
     petal_array = [arr[0], arr[1], arr[2], arr[3]]
     species = arr[4]
-    try:
-        for measurement in petal_array:
-            measurement = float(measurement)
-    except:
-        return False
     for measurement in petal_array:
+        if isinstance(measurement, str): return False
         if math.isnan(float(measurement)): return False
     if species not in {"Setosa", "Versicolor", "Virginica"}:
         return False
@@ -35,10 +31,8 @@ def check_feasibility(arr: np.ndarray) -> bool:
     petal_array = [arr[0], arr[1], arr[2], arr[3]]
 
     for petal in petal_array:
-        try:
-            petal = float(petal)
-        except:
-            return False
+        if isinstance(petal, str): pass
+        elif math.isnan(petal): return False
         if float(petal) < 0 or float(petal) > 15:
             return False
     return True
@@ -53,13 +47,15 @@ def mean(arr: list[float]) -> float:
 
 def calculate_avg_from_data(data: pd.DataFrame) -> list:
     df = data.transpose().values
+    print(df[0])
     def get_values(df, index):
         return [elem for elem in df[index] if isinstance(elem, float) and not math.isnan(elem)]
+    print(get_values(df, 0))
     return [mean(get_values(df, 0)), mean(get_values(df, 1)), mean(get_values(df, 2)), mean(get_values(df, 3))]
 
 
 def correct_unfeasible_data(data: pd.DataFrame) -> pd.DataFrame:
-    df = numerize(data)
+    df = data.copy(deep=True)
     avgs = calculate_avg_from_data(df)
     for iris in df.values:
         for i in range(len(iris)):
@@ -69,15 +65,24 @@ def correct_unfeasible_data(data: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def capitalize_class(data: pd.DataFrame) -> pd.DataFrame:
+    df = data.transpose()
+    for label in df.values[4]:
+        label = label.capitalize()
+    return df.transpose()
 
-df = pd.read_csv("./iris_with_errors.csv")
 
-correct_data = [iris for iris in df.values if check_correctness(iris) and check_feasibility(iris)]
+
+df = pd.read_csv("iris_with_errors.csv")
+corrected_df = capitalize_class(numerize(df))
+print(corrected_df.values[0])
+
+correct_data = [iris for iris in corrected_df.values if check_correctness(iris) and check_feasibility(iris)]
 outliers = [iris for iris in df.values if check_correctness(iris) and not check_feasibility(iris)]
 incorrect_data = [iris for iris in df.values if not check_correctness(iris)]
 print(f"Correct datapoints: {len(correct_data)}\nIncorrect datapoints: {len(incorrect_data)}")
 
-corrected = correct_unfeasible_data(df)
+corrected = correct_unfeasible_data(corrected_df)
 c_outliers = [iris for iris in corrected.values if check_correctness(iris) and not check_feasibility(iris)]
 print(c_outliers)
 
